fb7272ed451b1519aedf7355cb12fdca6b6534e3
Yefrin Pacheco <benigno.amaya1994@gmail.com>
2024-02-15 21:32:45 -0600

    Inicial

---
+++ app/src/main/java/com/uth/pm1examen1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.uth.pm1examen1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
