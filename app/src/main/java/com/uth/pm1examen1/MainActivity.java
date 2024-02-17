449a613cd4a4489f863ca14e7dbb53543d63b748
Damarislara10 <156867187+Damarislara10@users.noreply.github.com>
2024-02-16 23:00:11 -0600

    Clase Contacto

--- app/src/main/java/com/uth/pm1examen1/MainActivity.java
+++ app/src/main/java/com/uth/pm1examen1/MainActivity.java
@@ -5,10 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public class Pais {
+        private int id;
+        private String nombre;
 
+        public Pais(int id, String nombre) {
+            this.id = id;
+            this.nombre = nombre;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
